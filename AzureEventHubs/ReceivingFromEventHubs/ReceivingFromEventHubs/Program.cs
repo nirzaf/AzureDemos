d359f57ce5798392dd13ee5379c9a7bb39fb5893
nirzaf <mfmfazrin1986@gmail.com>
2021-09-19 20:42:24 +0530

    /swagger/v1/

--- AzureEventHubs/ReceivingFromEventHubs/ReceivingFromEventHubs/Program.cs
+++ AzureEventHubs/ReceivingFromEventHubs/ReceivingFromEventHubs/Program.cs
@@ -16,20 +16,13 @@ namespace SendingToEventHub
 		static async Task Main(string[] args)
 		{
 			Console.WriteLine("Starting our Event Hub Receiver");
-
-
-
 			string namespaceConnectionString = "Endpoint=sb://youtubeeventhubdemo.servicebus.windows.net/;SharedAccessKeyName=sendandreceive;SharedAccessKey=Lz65+NZ5N0zwZRkvih+8kXfuD0tYWV7Sk1nNdY+xA+0=;EntityPath=demoeventhub";
 			string eventHubName = "demoeventhub";
 
-
 			string blobConnectionString = "DefaultEndpointsProtocol=https;AccountName=eventhubstorageaccount3;AccountKey=ncQZvl23zgA6eS2+KUJ6IERd/emASeUFmBU2kVv/XYbepeA+h3HdyuGJ6g4WtFZ+gGOAfD6uP/tA9acXD29P8g==;EndpointSuffix=core.windows.net";
 			string containerName = "offsetcontainer";
-
-
-			BlobContainerClient blobContainerClient = new BlobContainerClient(blobConnectionString, containerName);
-
-			EventProcessorClient processor = new EventProcessorClient(blobContainerClient, "$Default", namespaceConnectionString, eventHubName);
+            BlobContainerClient blobContainerClient = new BlobContainerClient(blobConnectionString, containerName);
+            EventProcessorClient processor = new EventProcessorClient(blobContainerClient, "$Default", namespaceConnectionString, eventHubName);
 
 			processor.ProcessEventAsync += Processor_ProcessEventAsync;
 			processor.ProcessErrorAsync += Processor_ProcessErrorAsync;
@@ -37,14 +30,10 @@ namespace SendingToEventHub
 			await processor.StartProcessingAsync();
 			Console.WriteLine("Started the processor");
 
-
 			Console.ReadLine();
 			await processor.StopProcessingAsync();
 			Console.WriteLine("Started the processor");
-
-
-
-		}
+        }
 
 		private static Task Processor_ProcessErrorAsync(ProcessErrorEventArgs arg)
 		{
@@ -55,9 +44,7 @@ namespace SendingToEventHub
 		private static async Task Processor_ProcessEventAsync(ProcessEventArgs arg)
 		{
 			Console.WriteLine($"Event Received from Partition {arg.Partition.PartitionId}: {arg.Data.EventBody.ToString()}");
-
-			await arg.UpdateCheckpointAsync();
-			
-		}
+            await arg.UpdateCheckpointAsync();
+        }
 	}
 }
--- AzureEventHubs/ReceivingFromEventHubs/SendingToEventHub/Program.cs
+++ AzureEventHubs/ReceivingFromEventHubs/SendingToEventHub/Program.cs
@@ -13,10 +13,8 @@ namespace SendingToEventHub
         static async Task Main(string[] args)
         {
             Console.WriteLine("Starting our Event Hub Producer");
-
             string namespaceConnectionString = "Endpoint=sb://eventhubyoutubedemos2.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=KoaJlFSB5wTdAnwoHKrQA+fl2eoWHpjyBK9vkuo8aFw=";
             string eventHubName = "demoeventhub";
-
 
             await SendAFewMessages(namespaceConnectionString, eventHubName);
             Console.WriteLine("Messages Sent");
@@ -31,19 +29,15 @@ namespace SendingToEventHub
             EventHubProducerClient client = new EventHubProducerClient(namespaceConnectionString, eventHubName);
 
             //var batch = await client.CreateBatchAsync(new CreateBatchOptions { PartitionKey = "hello" });
-            List<EventData> events = new();
             EventData[] messages = new EventData[1];
-            for (int x = 0; x < 10; x++)
+            for (var x = 0; x < 10; x++)
             {
                 var greetingPart = (x % 2 == 0 ? "Hello " : "World! ");
                 var msgText = $"{ messageNumber++} - {greetingPart}";
-                var msg = new Azure.Messaging.EventHubs.EventData(msgText);
+                var msg = new EventData(msgText);
                 messages[0] = msg;
                 await client.SendAsync(messages, new SendEventOptions { PartitionId = (x%2).ToString()});
-
             }
-
-
         }
     }
 }
